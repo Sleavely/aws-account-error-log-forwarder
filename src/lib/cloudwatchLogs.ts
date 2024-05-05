c100876fe681753f5fd60e04387c55272cd599c2
Joakim Hedlund <contact@joakimhedlund.com>
2024-05-05 16:37:02 +0200

    fix(ts): typed response from unzipLogData

--- src/lib/cloudwatchLogs.ts
+++ src/lib/cloudwatchLogs.ts
@@ -2,7 +2,7 @@ import { gunzip } from 'zlib'
 
 export const unzipLogdata = async (
   data: AWSLambda.CloudWatchLogsEvent['awslogs']['data'],
-): Promise<unknown> => {
+): Promise<AWSLambda.CloudWatchLogsDecodedData> => {
   return await new Promise((resolve, reject) => {
     gunzip(Buffer.from(data, 'base64'), (err, res) => {
       if (err) { reject(err); return }
