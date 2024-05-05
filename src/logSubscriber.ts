04233469c7fbe65eccd2c39d161668c23af8917f
Joakim Hedlund <contact@joakimhedlund.com>
2024-05-05 16:39:04 +0200

    feat: parse messages and deliver via Discord Webhook

--- src/logSubscriber.ts
+++ src/logSubscriber.ts
@@ -1,19 +1,36 @@
-// const {
-//   DISCORD_WEBHOOK_URL = '',
-// } = process.env
+import { getLogStreamConsoleUrl, unzipLogdata } from './lib/cloudwatchLogs'
+import { discordWebhook } from './lib/discord'
+
+const {
+  DISCORD_WEBHOOK_URL = '',
+} = process.env
+
+const extractLogEventMessage = (input: string): string => {
+  try {
+    // If we can find the tab-delimited loglevel produced by AWS Lambda we'll strip that and anything preceeding it,
+    // retaining only the output from the application itself
+    const rightOfNativeLevel = input.replace(/^.*?\t(INFO|WARN(ING)?|ERROR)\t/, '').trim()
+    try {
+      // If the app printed JSON data we format it
+      return JSON.stringify(JSON.parse(rightOfNativeLevel), null, 2)
+    } catch (err) {
+      return rightOfNativeLevel
+    }
+  } catch (err) {
+    return input
+  }
+}
 
 export const handler = async (event: AWSLambda.CloudWatchLogsEvent): Promise<void> => {
-  // lets figre out what the event structure looks like
-  console.log(JSON.stringify(event, null, 2))
+  const log = await unzipLogdata(event.awslogs.data)
 
-  // const content = 'testing!'
-  // await fetch(DISCORD_WEBHOOK_URL, {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     content,
-  //   }),
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // })
+  await log.logEvents.reduce(async (previousPromise, logEvent) => {
+    await previousPromise
+    const logMessage = extractLogEventMessage(logEvent.message)
+
+    const content = `\`\`\`\n${logMessage}\n\`\`\`\n${getLogStreamConsoleUrl(log)}`
+    await discordWebhook(DISCORD_WEBHOOK_URL, {
+      content,
+    })
+  }, Promise.resolve())
 }
